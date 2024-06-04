81bf5329c9aed91b12c6f078dbb04dfd887ad326
seoyun610 <seoyun610@naver.com>
2024-06-04 15:49:08 +0900

    수정용

--- sportscenter/src/main/java/kr/project/sportscenter/util/LoginInterceptor.java
+++ sportscenter/src/main/java/kr/project/sportscenter/util/LoginInterceptor.java
@@ -1,38 +1,4 @@
-<<<<<<< HEAD
-package kr.project.sportscenter.util;
 
-import java.io.PrintWriter;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-import org.springframework.web.servlet.HandlerInterceptor;
-
-import kr.project.sportscenter.user.UserVO;
-
-public class LoginInterceptor implements HandlerInterceptor{
-	@Override
-	public boolean preHandle(HttpServletRequest request, 
-							HttpServletResponse response, 
-							Object handler)
-							throws Exception {
-		HttpSession sess = request.getSession();
-		UserVO login = (UserVO)sess.getAttribute("login");
-		if (login == null) {
-			response.setContentType("text/html; charset=utf-8");
-			PrintWriter out = response.getWriter();
-			out.print("<script>");
-			out.print("alert('로그인 후 사용가능합니다.');");
-			out.print("location.href='/user/login.do';");
-			out.print("</script>");
-			out.close();
-			return false; // 못가
-		}
-		return true; // 가던길가
-	}
-}
-=======
 package kr.project.sportscenter.util;
 
 import java.io.PrintWriter;
@@ -67,4 +33,3 @@ public class LoginInterceptor implements HandlerInterceptor{
 		return true; // 가던길가
 	}
 }
->>>>>>> branch 'master' of https://github.com/seoyun610/sportscenter2.git
